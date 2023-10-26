808eacd35a562f8c0adab2b932604bdf71bc263d
Kate Mashko <katika.life@gmail.com>
2023-10-26 10:16:53 +0300

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    
+    let openS= {
+        '[': ']',
+        '{': '}',
+        '(': ')',
+      };
+  
+      let stack = [];
+      let ret = '';
+  
+      for(let i = 0; i < str.length; i++){
+        let s = str[i];
+  
+        if(openS[s]){
+          stack.push(s);
+        } else {
+          let br = stack.pop();
+          s = openS[br];
+        }
+  
+        ret += s;
+      }
+      if (stack.length > 0){
+        return false
+      } else {return true}
+    }
+
+  
+
